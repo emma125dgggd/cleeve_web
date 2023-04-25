5bcaf15c520b7d5da83178c7791bd86637cde6f9
emma125dgggd <69827304+emma125dgggd@users.noreply.github.com>
2023-04-25 14:36:15 -0700

    Update main.py

--- main.py
+++ main.py
@@ -18,22 +18,7 @@ import tensorflow as tf
 import matplotlib.pyplot as plt
 from lib import *
 
-
-
-
-#functions
-
-#file saver
-def save_uploaded_file(uploadedfile):
-    with open(os.path.join(uploadedfile.name),"wb") as f:
-        f.write(uploadedfile.getbuffer())
-
-
- 
-
-
-
-
+#Streamlit UI
 st.title('Cleeve AI')
 
 st.markdown(
@@ -49,47 +34,6 @@ st.markdown(
     """,
     unsafe_allow_html=True,
 )
-
-
-
-@st.cache_data
-def image_resize(image, width=None, height=None, inter=cv2.INTER_AREA):
-    # initialize the dimensions of the image to be resized and
-    # grab the image size
-    dim = None
-    (h, w) = image.shape[:2]
-
-    # if both the width and height are None, then return the
-    # original image
-    if width is None and height is None:
-        return image
-
-    # check to see if the width is None
-    if width is None:
-        # calculate the ratio of the height and construct the
-        # dimensions
-        r = height / float(h)
-        dim = (int(w * r), height)
-
-    # otherwise, the height is None
-    else:
-        # calculate the ratio of the width and construct the
-        # dimensions
-        r = width / float(w)
-        dim = (width, int(h * r))
-
-    # resize the image
-    resized = cv2.resize(image, dim, interpolation=inter)
-
-    # return the resized image
-    return resized
-
-
-
-
-
-
-    
 
 
 # Define icon menu options
