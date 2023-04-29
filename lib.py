44fcf36fd80384023d03ad961efc8de41d9be7c9
emma125dgggd <69827304+emma125dgggd@users.noreply.github.com>
2023-04-28 20:02:16 -0700

    Update lib.py

--- lib.py
+++ lib.py
@@ -92,19 +92,11 @@ def letterbox(im, new_shape=(640, 640), color=(114, 114, 114), auto=True, scaleu
         
 def process_slide(uploaded_image, UploadedFile):
           # Load the TFLite model and allocate tensors.
-          interpreter = tf.lite.Interpreter(model_path = "yolov7.tflite")
+          interpreter = tf.lite.Interpreter(model_path = "yolov7_model.tflite")
 
 
           #Name of the classes according to class indices.
-          names = ['person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus', 'train', 'truck', 'boat', 'traffic light',
-               'fire hydrant', 'stop sign', 'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse', 'sheep', 'cow',
-               'elephant', 'bear', 'zebra', 'giraffe', 'backpack', 'umbrella', 'handbag', 'tie', 'suitcase', 'frisbee',
-               'skis', 'snowboard', 'sports ball', 'kite', 'baseball bat', 'baseball glove', 'skateboard', 'surfboard',
-               'tennis racket', 'bottle', 'wine glass', 'cup', 'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple',
-               'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza', 'donut', 'cake', 'chair', 'couch',
-               'potted plant', 'bed', 'dining table', 'toilet', 'tv', 'laptop', 'mouse', 'remote', 'keyboard', 'cell phone',
-               'microwave', 'oven', 'toaster', 'sink', 'refrigerator', 'book', 'clock', 'vase', 'scissors', 'teddy bear',
-               'hair drier', 'toothbrush']
+          names = ['pf']
 
           #Creating random colors for bounding box visualization.
           colors = {name:[random.randint(0, 255) for _ in range(3)] for i,name in enumerate(names)}
