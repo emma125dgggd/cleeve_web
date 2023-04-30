1906e152bf3c56af4ac1471cd0c0d8c3f85351d8
emma125dgggd <69827304+emma125dgggd@users.noreply.github.com>
2023-04-30 03:35:54 -0700

    Update lib.py

--- lib.py
+++ lib.py
@@ -171,7 +171,7 @@ def process_slide(uploaded_image, UploadedFile):
           print(img_path)        
           cv2.imwrite(img_path, ori_images[0]) 
           if "img_path" not in st.session_state:
-              st.session_state.img_path = []
+              #st.session_state.img_path = []
               st.session_state.img_path= st.session_state.img_path.append(img_path)
           st.image(ori_images[0], use_column_width=True, channels="RGB")
           st.write(img_path)
