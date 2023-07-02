fcdd3f4626511b70695a84eeca80bdfda8bb35be
emma125dgggd <69827304+emma125dgggd@users.noreply.github.com>
2023-07-02 05:09:35 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,22 @@
+from fastapi import FastAPI, HTTPException
+from fastapi import UploadFile
+import uvicorn
+import numpy as np
+import json
+import lib
 
+app = FastAPI()
+
+@app.post("/process-slide")
+async def process_slide_api(json_obj: dict):
+    try:
+        response = process_slide(json_obj)
+
+        # Return the response as JSON
+        return response
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+      
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)  
