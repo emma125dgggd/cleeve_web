270e2cf63a132488eb96e2f1242066d0267a89ea
emma125dgggd <69827304+emma125dgggd@users.noreply.github.com>
2023-07-08 05:39:23 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,25 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, File
 from fastapi import UploadFile
+import base64
 import uvicorn
 import numpy as np
 import json
-import lib
+from lib import *
 
 app = FastAPI()
 
 @app.post("/process-slide")
-async def process_slide_api(json_obj: dict):
+async def process_slide_api(image: UploadFile = File(...)):
     try:
-        count, image = process_slide(json_obj)
+        file_content = await image.read()
+        encoded_image = base64.b64encode(file_content).decode("utf-8")
+
+        # Create a JSON object with the base64-encoded image data
+        json_obj = {"image_data": encoded_image,
+                    "filename" : image.filename
+                    }
+        json_data = json.dumps(json_obj)
+        count, image = process_slide(json_data)
 
         # Return the response as JSON
         return count, image
@@ -18,5 +27,4 @@ async def process_slide_api(json_obj: dict):
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
       
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)  
+
